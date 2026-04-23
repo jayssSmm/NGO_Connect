6858d79d85dfd1d8e391f5ad3ed769d3a0f02b14
Samman jaiswal <214768065+jayssSmm@users.noreply.github.com>
2026-04-23 11:04:39 +0000

    changed the user schema

--- app/models/user.py
+++ app/models/user.py
@@ -1,10 +1,11 @@
 from app.extensions import db
 from sqlalchemy import func
+from sqlalchemy.dialects.postgresql import UUID
 
 class User(db.Model):
     __tablename__ = "users"
 
-    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(UUID(as_uuid=True), primary_key=True, server_default=db.text("gen_random_uuid()"))
     email = db.Column(db.String(100), unique=True, nullable=False)
     password_hash = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
