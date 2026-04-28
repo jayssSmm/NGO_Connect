dda81be9cd9aee540ff13764b5497057829af5c0
Wertegved <jatin.sharma062007@gmail.com>
2026-04-28 23:12:54 +0530

    Delete app directory

--- app/blueprints/dashboard.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint,render_template,request
-
-bp = Blueprint("dashboard",__name__)
-
-@bp.route('/dashboard')
-def dashboard():
-    return render_template('Dashboard.html')
--- app/blueprints/donate.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint,render_template,request
-
-bp = Blueprint("donate",__name__)
-
-@bp.route('/donate',methods=['GET','POST'])
-def donate():
-    if request.method == 'GET':
-        return render_template("Donate.html")
-    
